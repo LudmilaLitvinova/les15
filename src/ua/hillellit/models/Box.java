15772bd4ba6828ac69e9b1da7483cebc4d1e6f85
LudmilaLitvinova <ludik-1984@ukr.net>
2022-10-17 10:19:39 +0300

    Null check fix

--- src/ua/hillellit/models/Box.java
+++ src/ua/hillellit/models/Box.java
@@ -4,29 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 import ua.hillellit.models.fruits.Fruit;
 
-public class Box <T extends  Fruit> {
+public class Box<T extends Fruit> {
 
   ArrayList<T> arrayList = new ArrayList<>();
   T fruit;
 
   public void addFruit(T fruit) {
-      this.fruit=fruit;
+    if (fruit != null) {
+      this.fruit = fruit;
       arrayList.add(fruit);
     }
+  }
 
   public void addFruits(List<T> fruits) {
-    this.fruit = fruits.get(0);
-    arrayList.addAll(fruits);
+    if (fruits != null) {
+      this.fruit = fruits.get(0);
+      arrayList.addAll(fruits);
+    }
   }
 
   public float getWeight() {
-    return arrayList.size() * fruit.weight;
+    if (arrayList != null && fruit != null) {
+      return arrayList.size() * fruit.weight;
+    }
+    return 0;
   }
 
   public boolean compare(Box box) {
-    return this.getWeight() == box.getWeight();
+    if (box != null) {
+      return this.getWeight() == box.getWeight();
+    }
+    return false;
   }
-  public void merge(Box<T>box){
-    this.arrayList.addAll(box.arrayList);
+
+  public void merge(Box<T> box) {
+    if (box != null) {
+      this.arrayList.addAll(box.arrayList);
+    }
   }
 }
